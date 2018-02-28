b41b682fbabadf19bab0e33efc5f2bf9bb58e996
Michael David Wilson <michaelwilson1991@gmail.com>
2018-02-28 23:26:57 +0800

    Fixed bug in grandmeans for error bar calculation
    * The error bar calculations were incorrectly including the Blank condition twice in several of the measures from a copy and paste error.
    
    e.g.
    
    ```
    \# Incorrect
    (mean(c(exp2.resumptionMisses$None,exp2.resumptionMisses$Blank,exp2.resumptionMisses$None)))
    
    \# Correct
    (mean(c(exp2.resumptionMisses$ATC,exp2.resumptionMisses$Blank,exp2.resumptionMisses$None)))
    ```

--- experiment1/src/exp1-confidence-intervals.R
+++ experiment1/src/exp1-confidence-intervals.R
@@ -15,14 +15,14 @@ exp1.wsci.restime.Nback <- 1.96*(describe(
 exp1.wsci.restime.blank <- 1.96*(describe(
   exp1.resumptionTime[,3] -
     rowMeans(exp1.resumptionTime[,2:4]) +
-    (mean(c(exp1.resumptionTime$Blank,exp1.resumptionTime$Blank,exp1.resumptionTime$None)))
+    (mean(c(exp1.resumptionTime$Nback,exp1.resumptionTime$Blank,exp1.resumptionTime$None)))
 )$se
 )
 
 exp1.wsci.restime.none <- 1.96*(describe(
   exp1.resumptionTime[,4] -
     rowMeans(exp1.resumptionTime[,2:4]) +
-    (mean(c(exp1.resumptionTime$None,exp1.resumptionTime$Blank,exp1.resumptionTime$None)))
+    (mean(c(exp1.resumptionTime$Nback,exp1.resumptionTime$Blank,exp1.resumptionTime$None)))
 )$se
 )
 
@@ -37,14 +37,14 @@ exp1.wsci.resfail.Nback <- 1.96*(describe(
 exp1.wsci.resfail.blank <- 1.96*(describe(
   exp1.resumptionMisses[,3] -
     rowMeans(exp1.resumptionMisses[,2:4]) +
-    (mean(c(exp1.resumptionMisses$Blank,exp1.resumptionMisses$Blank,exp1.resumptionMisses$None)))
+    (mean(c(exp1.resumptionMisses$Nback,exp1.resumptionMisses$Blank,exp1.resumptionMisses$None)))
 )$se
 )
 
 exp1.wsci.resfail.none <- 1.96*(describe(
   exp1.resumptionMisses[,4] -
     rowMeans(exp1.resumptionMisses[,2:4]) +
-    (mean(c(exp1.resumptionMisses$None,exp1.resumptionMisses$Blank,exp1.resumptionMisses$None)))
+    (mean(c(exp1.resumptionMisses$Nback,exp1.resumptionMisses$Blank,exp1.resumptionMisses$None)))
 )$se
 )
 
@@ -59,14 +59,14 @@ exp1.wsci.defhomiss.Nback <- 1.96*(describe(
 exp1.wsci.defhomiss.blank <- 1.96*(describe(
   exp1.deferredHandoffMiss[,3] -
     rowMeans(exp1.deferredHandoffMiss[,2:4]) +
-    (mean(c(exp1.deferredHandoffMiss$Blank,exp1.deferredHandoffMiss$Blank,exp1.deferredHandoffMiss$None)))
+    (mean(c(exp1.deferredHandoffMiss$Nback,exp1.deferredHandoffMiss$Blank,exp1.deferredHandoffMiss$None)))
 )$se
 )
 
 exp1.wsci.defhomiss.none <- 1.96*(describe(
   exp1.deferredHandoffMiss[,4] -
     rowMeans(exp1.deferredHandoffMiss[,2:4]) +
-    (mean(c(exp1.deferredHandoffMiss$None,exp1.deferredHandoffMiss$Blank,exp1.deferredHandoffMiss$None)))
+    (mean(c(exp1.deferredHandoffMiss$Nback,exp1.deferredHandoffMiss$Blank,exp1.deferredHandoffMiss$None)))
 )$se
 )
 
@@ -81,13 +81,13 @@ exp1.wsci.defhotime.Nback <- 1.96*(describe(
 exp1.wsci.defhotime.blank <- 1.96*(describe(
   exp1.deferredHandoffTime[,3] -
     rowMeans(exp1.deferredHandoffTime[,2:4]) +
-    (mean(c(exp1.deferredHandoffTime$Blank,exp1.deferredHandoffTime$Blank,exp1.deferredHandoffTime$None)))
+    (mean(c(exp1.deferredHandoffTime$Nback,exp1.deferredHandoffTime$Blank,exp1.deferredHandoffTime$None)))
 )$se
 )
 
 exp1.wsci.defhotime.none <- 1.96*(describe(
   exp1.deferredHandoffTime[,4] -
     rowMeans(exp1.deferredHandoffTime[,2:4]) +
-    (mean(c(exp1.deferredHandoffTime$None,exp1.deferredHandoffTime$Blank,exp1.deferredHandoffTime$None)))
+    (mean(c(exp1.deferredHandoffTime$Nback,exp1.deferredHandoffTime$Blank,exp1.deferredHandoffTime$None)))
 )$se
 )
--- experiment2/src/exp2-confidence-intervals.R
+++ experiment2/src/exp2-confidence-intervals.R
@@ -15,14 +15,14 @@ exp2.wsci.restime.atc <- 1.96*(describe(
 exp2.wsci.restime.blank <- 1.96*(describe(
   exp2.resumptionTime[,3] -
     rowMeans(exp2.resumptionTime[,2:4]) +
-    (mean(c(exp2.resumptionTime$Blank,exp2.resumptionTime$Blank,exp2.resumptionTime$None)))
+    (mean(c(exp2.resumptionTime$ATC,exp2.resumptionTime$Blank,exp2.resumptionTime$None)))
 )$se
 )
 
 exp2.wsci.restime.none <- 1.96*(describe(
   exp2.resumptionTime[,4] -
     rowMeans(exp2.resumptionTime[,2:4]) +
-    (mean(c(exp2.resumptionTime$None,exp2.resumptionTime$Blank,exp2.resumptionTime$None)))
+    (mean(c(exp2.resumptionTime$ATC,exp2.resumptionTime$Blank,exp2.resumptionTime$None)))
 )$se
 )
 
@@ -37,14 +37,14 @@ exp2.wsci.resfail.atc <- 1.96*(describe(
 exp2.wsci.resfail.blank <- 1.96*(describe(
   exp2.resumptionMisses[,3] -
     rowMeans(exp2.resumptionMisses[,2:4]) +
-    (mean(c(exp2.resumptionMisses$Blank,exp2.resumptionMisses$Blank,exp2.resumptionMisses$None)))
+    (mean(c(exp2.resumptionMisses$ATC,exp2.resumptionMisses$Blank,exp2.resumptionMisses$None)))
 )$se
 )
 
 exp2.wsci.resfail.none <- 1.96*(describe(
   exp2.resumptionMisses[,4] -
     rowMeans(exp2.resumptionMisses[,2:4]) +
-    (mean(c(exp2.resumptionMisses$None,exp2.resumptionMisses$Blank,exp2.resumptionMisses$None)))
+    (mean(c(exp2.resumptionMisses$ATC,exp2.resumptionMisses$Blank,exp2.resumptionMisses$None)))
 )$se
 )
 
@@ -59,14 +59,14 @@ exp2.wsci.defhomiss.atc <- 1.96*(describe(
 exp2.wsci.defhomiss.blank <- 1.96*(describe(
   exp2.deferredHandoffMiss[,3] -
     rowMeans(exp2.deferredHandoffMiss[,2:4]) +
-    (mean(c(exp2.deferredHandoffMiss$Blank,exp2.deferredHandoffMiss$Blank,exp2.deferredHandoffMiss$None)))
+    (mean(c(exp2.deferredHandoffMiss$ATC,exp2.deferredHandoffMiss$Blank,exp2.deferredHandoffMiss$None)))
 )$se
 )
 
 exp2.wsci.defhomiss.none <- 1.96*(describe(
   exp2.deferredHandoffMiss[,4] -
     rowMeans(exp2.deferredHandoffMiss[,2:4]) +
-    (mean(c(exp2.deferredHandoffMiss$None,exp2.deferredHandoffMiss$Blank,exp2.deferredHandoffMiss$None)))
+    (mean(c(exp2.deferredHandoffMiss$ATC,exp2.deferredHandoffMiss$Blank,exp2.deferredHandoffMiss$None)))
 )$se
 )
 
@@ -81,13 +81,13 @@ exp2.wsci.defhotime.atc <- 1.96*(describe(
 exp2.wsci.defhotime.blank <- 1.96*(describe(
   exp2.deferredHandoffTime[,3] -
     rowMeans(exp2.deferredHandoffTime[,2:4]) +
-    (mean(c(exp2.deferredHandoffTime$Blank,exp2.deferredHandoffTime$Blank,exp2.deferredHandoffTime$None)))
+    (mean(c(exp2.deferredHandoffTime$ATC,exp2.deferredHandoffTime$Blank,exp2.deferredHandoffTime$None)))
 )$se
 )
 
 exp2.wsci.defhotime.none <- 1.96*(describe(
   exp2.deferredHandoffTime[,4] -
     rowMeans(exp2.deferredHandoffTime[,2:4]) +
-    (mean(c(exp2.deferredHandoffTime$None,exp2.deferredHandoffTime$Blank,exp2.deferredHandoffTime$None)))
+    (mean(c(exp2.deferredHandoffTime$ATC,exp2.deferredHandoffTime$Blank,exp2.deferredHandoffTime$None)))
 )$se
 )
