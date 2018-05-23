6fb24900ae7f32de301c418897067e14c190fc98
Michael Wilson <michael@humanfactors.io>
2018-05-23 11:11:44 +0800

    Added sRGB colors for figures

---
+++ figures/rgbconvert.bash
@@ -0,0 +1,4 @@
+convert ./joined/exp1-dc.png -type truecolor ./joined/exp1-dc-sRGB.png
+convert ./joined/exp1-dh.png -type truecolor ./joined/exp1-dh-sRGB.png
+convert ./joined/exp2-dc.png -type truecolor ./joined/exp2-dc-sRGB.png
+convert ./joined/exp2-dh.png -type truecolor ./joined/exp2-dh-sRGB.png
